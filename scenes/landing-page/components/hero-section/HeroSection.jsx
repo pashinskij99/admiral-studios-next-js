0d3606bf9a6392d12af826bbe1fd46d1503c34b9
pashinskij99 <pashinskij99@icloud.com>
2023-07-19 17:52:48 +0300

    fx

--- scenes/landing-page/components/hero-section/HeroSection.jsx
+++ scenes/landing-page/components/hero-section/HeroSection.jsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 
 import { Button } from '../../../../components'
-import Wave from '../../../../components/wave/Wave'
+// import Wave from '../../../../components/wave/Wave'
 
 import heroBack from '../../../../images/landing/hero/hero-back-new.webp'
 import heroBackMobile from '../../../../images/landing/hero/hero-back-mobile.webp'
