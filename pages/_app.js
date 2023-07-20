1e093d6e00bfc85dd1fdacab96c92a572ea5320b
pashinskij99 <pashinskij99@icloud.com>
2023-07-20 18:22:03 +0300

    fx

--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,7 @@
 import '../styles/globals.scss'
-// import { useEffect } from 'react'
-// import TagManager from 'react-gtm-module'
-// import fonts from '@next/font/google'
-
-// const manrope = fonts.Manrope({
-//   weight: ['700'],
-//   display: 'swap',
-//   subsets: ['latin'],
-// })
 
 const MyApp = ({ Component, pageProps }) => {
-  // console.log(manrope)
-  return (
-    // <>
-    //   <style jsx global>{`
-    //     html {
-    //       font-family: ${manrope.style.fontFamily};
-    //     }
-    //   `}</style>
-    // </>
-    <Component {...pageProps} />
-  )
+  return <Component {...pageProps} />
 }
 
 export default MyApp
