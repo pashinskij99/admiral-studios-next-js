43789f521e9eba27fb94f168c9f885fb41bdb717
pashinskij99 <pashinskij99@icloud.com>
2023-07-20 17:32:32 +0300

    fx

--- scenes/landing-page/LandingPage.jsx
+++ scenes/landing-page/LandingPage.jsx
@@ -1,45 +1,45 @@
-// import { FreeAudit, HeroSection, Map } from './components'
-// import dynamic from 'next/dynamic'
+import { FreeAudit, HeroSection, Map } from './components'
+import dynamic from 'next/dynamic'
 
-// const DynamicAccordion = dynamic(
-//   () => import('./components/accordion/Accordion').then((data) => data.default),
-//   {
-//     ssr: false,
-//   }
-// )
+const DynamicAccordion = dynamic(
+  () => import('./components/accordion/Accordion').then((data) => data.default),
+  {
+    ssr: false,
+  }
+)
 
-// const DynamicSitesList = dynamic(
-//   () =>
-//     import('./components/sites-list/SitesList').then((data) => data.default),
-//   {
-//     ssr: false,
-//   }
-// )
+const DynamicSitesList = dynamic(
+  () =>
+    import('./components/sites-list/SitesList').then((data) => data.default),
+  {
+    ssr: false,
+  }
+)
 
-// const DynamicAboutUs = dynamic(
-//   () => import('./components/about-us/AboutUs').then((data) => data.default),
-//   {
-//     ssr: false,
-//   }
-// )
+const DynamicAboutUs = dynamic(
+  () => import('./components/about-us/AboutUs').then((data) => data.default),
+  {
+    ssr: false,
+  }
+)
 
-// const DynamicWeCan = dynamic(
-//   () => import('./components/we-can/WeCan').then((data) => data.default),
-//   {
-//     ssr: false,
-//   }
-// )
+const DynamicWeCan = dynamic(
+  () => import('./components/we-can/WeCan').then((data) => data.default),
+  {
+    ssr: false,
+  }
+)
 
 const LandingPage = () => {
   return (
     <div>
-      {/* <HeroSection />
+      <HeroSection />
       <Map />
       <FreeAudit />
       <DynamicSitesList />
       <DynamicAccordion />
       <DynamicAboutUs />
-      <DynamicWeCan /> */}
+      <DynamicWeCan />
     </div>
   )
 }
